4333ae74622b2eabe9c2c6ea72051c5a346c6ae5
Avujcic1 <44148907+Avujcic1@users.noreply.github.com>
2018-12-21 13:45:56 +0100

    Implementirane metode

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Drzava {
+
+    private int br;
+    private String ime;
+    private Grad glGrad;
+
+    public Drzava() {}
+
+    public Drzava(int br, String ime, Grad glGrad) {
+        this.br = br;
+        this.ime = ime;
+        this.glGrad = glGrad;
+    }
+
+    public int getBr() {
+        return br;
+    }
+
+    public void setBr(int br) {
+        this.br = br;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public Grad getGlGrad() {
+        return glGrad;
+    }
+
+
+    public void setGlGrad(Grad glGrad) {
+        this.glGrad = glGrad;
+    }
 }
