c255ceed8da8ab3af37976790570cb5348f12d60
Avujcic1 <44148907+Avujcic1@users.noreply.github.com>
2018-12-21 13:45:38 +0100

    Implementirane metode

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,42 +1,85 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class GeografijaDAO {
 
-    public static void removeInstance() {
+    private ArrayList<Grad> gradovi;
+    private ArrayList<Drzava> drzave;
+    private static GeografijaDAO instanca = null;
+    private Connection veza;
 
+    private static void initialize() {
+        instanca = new GeografijaDAO();
+    }
+
+    public static void removeInstance() {
+        instanca = null;
     }
 
     public static GeografijaDAO getInstance() {
-
+        if(instanca != null) return instanca;
+        else initialize();
+        return instanca;
     }
 
     public Grad glavnigrad(String drzava) {
-
+        for(int i=0; i<drzave.size(); i++) {
+            if(drzave.get(i).getIme().equals(drzava)) return drzave.get(i).getGlGrad();
+        }
+        return null;
     }
 
     public ArrayList<Grad> gradovi() {
-
+        return null;
     }
 
-    public Drzava nadjiDrzava(String drzava) {
-
+    public Drzava nadjiDrzavu(String drzava) {
+        for (int i = 0; i < drzave.size(); i++) {
+            if (drzave.get(i).getIme().equals(drzava)) return drzave.get(i);
+        }
+        return null;
     }
 
-    public void obrisiDrzavu(String drzava) {
-
+    public void obrisiDrzavu(String drzava) throws IllegalArgumentException {
+        for (int i = 0; i < drzave.size(); i++) {
+            if (drzave.get(i).getIme().equals(drzava)) {
+                drzave.remove(i);
+                break;
+            }
+        }
+        throw new IllegalArgumentException("Drzava nije pronadjena");
     }
 
-    public void dodajGrad(String grad) {
-
+    public void dodajGrad(Grad grad) throws IllegalArgumentException {
+        if(gradovi.contains(grad)) throw new IllegalArgumentException("Grad je postojeci");
+        gradovi.add(grad);
     }
 
-    public void izmjeniGrad(String grad) {
-
+    public void izmijeniGrad(Grad grad) throws IllegalArgumentException {
+        for (int i = 0; i < gradovi.size(); i++) {
+            if (gradovi.get(i).equals(grad)) {
+                gradovi.get(i).setIme(grad.getIme());
+                gradovi.get(i).setBrStanovnika(grad.getBrStanovnika());
+                gradovi.get(i).setDrzava(grad.getDrzava());
+                gradovi.get(i).setBr(grad.getBr());
+                break;
+            }
+        }
+        throw new IllegalArgumentException("Grad je postojeci");
     }
 
+    public void dodajDrzavu(Drzava drzava) throws IllegalArgumentException{
+        if(drzave.contains(drzava)) throw new IllegalArgumentException("Drzava je postojeca");
+        drzave.add(drzava);
+    }
 
-
-
+    public Grad glGrad(String drzava) {
+        for (int i = 0; i < drzave.size(); i++) {
+            if (drzave.get(i).getIme().equals(drzava)) return drzave.get(i).getGlGrad();
+        }
+        return null;
+    }
 }
+
--- src/ba/unsa/etf/rpr/Grad.java
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -2,28 +2,50 @@ package ba.unsa.etf.rpr;
 
 public class Grad {
 
+    private int br;
+    private int brStanovnika;
+    private String ime;
+    private Drzava drzave;
+
+    public Grad() {}
+
+    public Grad(int br, int brStanovnika, String ime, Drzava drzava) {
+        this.br = br;
+        this.brStanovnika = brStanovnika;
+        this.ime = ime;
+        this.drzave = drzava;
+    }
+
     public void setIme(String ime) {
-
+        this.ime = ime;
     }
 
-    public void setStanovnistvo(int brStanovnika) {
-
+    public void setBrStanovnika(int brStanovnika) {
+        this.brStanovnika = brStanovnika;
     }
 
-    public void setDrzavu(Drzava drzava) {
-
+    public void setDrzava(Drzava drzava) {
+        this.drzave = drzava;
     }
 
     public String getIme() {
-
+        return ime;
     }
 
     public int getBrStanovnika() {
-
+        return brStanovnika;
     }
 
-    public Grad getDrzava() {
+    public Drzava getDrzava() {
+        return drzave;
+    }
 
+    public void setBr(int br) {
+        this.br = br;
+    }
+
+    public int getBr() {
+        return br;
     }
 
 }
