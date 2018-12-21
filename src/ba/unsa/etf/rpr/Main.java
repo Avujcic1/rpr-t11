b58a747361cea2743b7357962c0363b42a8271da
avujcic1 <avujcic1@etf.unsa.ba>
2018-12-21 17:46:49 +0100

    prepravljen main za poziv i izvrsavanje

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,20 +1,28 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static GeografijaDAO dataBase = GeografijaDAO.getInstanca();
 
     public static void main(String[] args) {
-        System.out.println("Gradovi su:\n" + ispisiGradove());
+
+        System.out.println("Gradovi:\n" + ispisiGradove());
         glavniGrad();
     }
 
-    private static String ispisiGradove() {
-        return null;
+    private static void glavniGrad() {
+        Scanner ulaz = new Scanner(System.in);
+        String d = ulaz.nextLine();
+        var grad = dataBase.glavniGrad(d);
+        System.out.println("Glavni grad države " + grad.getDrzava().getIme() + " je " + grad.getIme());
     }
 
-    private static void glavniGrad() {
-
+    public static String ispisiGradove() {
+        var gradovi = dataBase.gradovi();
+        String result = "";
+        for (var grad : gradovi)
+            result += grad.toString() + "\n";
+        return result;
     }
 }
